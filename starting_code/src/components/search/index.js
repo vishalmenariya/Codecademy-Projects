86f6283ada7fff77068041d2840176232f35ef16
vishalmenariya <vishal.menariya@intimetec.com>
2026-03-20 12:04:21 +0530

    setup the routing of the project

---
+++ starting_code/src/components/search/index.js
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react';
+// Import createSearchParams
+import { createSearchParams, useSearchParams } from 'react-router-dom';
+// Import useNavigate
+import {  useNavigate } from 'react-router-dom';
+
+const Search = () => {
+
+  // get navigate function
+  const navigate = useNavigate();
+
+  const searchInputRef = useRef();
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+
+    const searchQuery = {
+      name: searchInputRef.current.value
+    }
+
+    // use createSearchParams 
+    const query = createSearchParams(searchQuery);
+
+    // imperatively redirect with useNavigate() returned function
+    navigate({
+      pathname: '/search',
+      search: `?${query}`
+    });
+  };
+
+  return (
+    <form onSubmit={onSearchHandler} className="search-form">
+      <input type="text" className="search" ref={searchInputRef} />
+      <button type="submit" className="search-button">
+        🔎
+      </button>
+    </form>
+  );
+};
+
+export default Search;
